015ea26263a262939b0288fe8ed836c7099a59c4
IlkaRick <62770826+IlkaRick@users.noreply.github.com>
2022-01-24 19:20:39 +0000

    Update astronomical_image_processing.py

--- astronomical_image_processing.py
+++ astronomical_image_processing.py
@@ -21,55 +21,97 @@ hdulist = fits.open(r"C:\Users\Yi Gan\Documents\Y3 Lab\A1 - astronomical image p
 image = hdulist[0].data
 plt.hist(image)
 
-pix = []
-for i in range(len(image)):
-    for j in range(len(image[0])):
-        pix.append(image[i,j])
-        
-plt.hist(pix,bins=1000)
+#masked right regions
+x, y, width, height, angle = np.loadtxt(r"C:\Users\Yi Gan\Documents\Y3 Lab\A1 - astronomical image processing\mask_1.txt", delimiter=' ', unpack = True)
+mask= np.ones((4611,2570))
+for k in range(len(x)):
+    xcentrefloat = float(x[k]) # Convert data from file to floats 
+    ycentrefloat = float(y[k])
+    widthfloat = float(width[k])
+    heightfloat = float(height[k])
+    xcentre = int(xcentrefloat) - 1 # Convert floats to intgers
+    ycentre = int(ycentrefloat) - 1
+    width1 = (int(widthfloat) + 1)/2 # Divide wdith and height by 2 to add and subtract from centres
+    height1 = (int(heightfloat) + 1)/2    
+    widthedge1 = xcentre - width1 # Find edges of regions
+    widthedge2 = xcentre + width1
+    heightedge1 = ycentre - height1
+    heightedge2 = ycentre + height1
+    for i in range (4611):
+        for j in range (2570):
+            if i >= heightedge1 and i <=heightedge2:
+                if j >= widthedge1 and j <= widthedge2:
+                    mask[i][j] = 0
+print(mask)
+print('right regions succeed')
+#masked left regions
+x, y, width, height, angle = sp.loadtxt(r"C:\Users\Yi Gan\Documents\Y3 Lab\A1 - astronomical image processing\left_regions.txt",dtype = str, unpack = True)
+for k in range(len(x)):
+    xcentrefloat = float(x[k]) # Convert data from file to floats 
+    ycentrefloat = float(y[k])
+    widthfloat = float(width[k])
+    heightfloat = float(height[k])
+    xcentre = int(xcentrefloat) - 1 # Convert floats to intgers
+    ycentre = int(ycentrefloat)- 1
+    width1 = (int(widthfloat) + 1)/2 # Divide wdith and height by 2 to add and subtract from centres
+    height1 = (int(heightfloat) + 1)/2    
+    widthedge1 = xcentre - width1 # Find edges of regions
+    widthedge2 = xcentre + width1
+    heightedge1 = ycentre - height1
+    heightedge2 = ycentre + height1
+    for i in range (4611):
+        for j in range (2570):
+            if i >= heightedge1 and i <=heightedge2:
+                if j >= widthedge1 and j <= widthedge2:
+                    mask[i][j] = 0
+print(mask)
+print('run left region succeed')
+#masked bigger area of the edges to get rid of the strange peak at 3421
+x, y, width, height, angle = sp.loadtxt(r"C:\Users\Yi Gan\Documents\Y3 Lab\A1 - astronomical image processing\mask_3.txt",dtype = str, unpack = True)
+for k in range(len(x)):
+    xcentrefloat = float(x[k]) # Convert data from file to floats 
+    ycentrefloat = float(y[k])
+    widthfloat = float(width[k])
+    heightfloat = float(height[k])
+    xcentre = int(xcentrefloat) - 1# Convert floats to intgers
+    ycentre = int(ycentrefloat) - 1
+    width1 = (int(widthfloat) + 1)/2 # Divide wdith and height by 2 to add and subtract from centres
+    height1 = (int(heightfloat) + 1)/2    
+    widthedge1 = xcentre - width1 # Find edges of regions
+    widthedge2 = xcentre + width1
+    heightedge1 = ycentre - height1
+    heightedge2 = ycentre + height1
+    for i in range (4611):
+        for j in range (2570):
+            if i >= heightedge1 and i <=heightedge2:
+                if j >= widthedge1 and j <= widthedge2:
+                    mask[i][j] = 0
+print(mask)
+print('run background/edges succeed')
 #%%
-zoom = []
-for p in range(len(pix)):
-    if pix[p] >=3300 and pix[p] <= 3600:
-        zoom.append(pix[p])
-
-
-mean = np.mean(zoom)
-sigma = []
-for k in range(len(zoom)):
-    sigma1 = (zoom[k]-mean)**2/len(zoom)
-    sigma.append(sigma1)
-guess_sigma = np.mean(sigma)
-values, bins, patches = plt.hist(zoom, bins = 300)
-test_mean = mean
-test_sigma = 50
-test_norm = 1e8
-def gauss_function(x, sigma, mu, norm):
-    gaus = norm*(1/(sigma*(sp.sqrt(2*sp.pi))))*(sp.exp(-(1/2)*((x-mu)/sigma)**2))
-    return(gaus)
-p0 = [test_sigma,test_mean,test_norm]
-fit_gauss, fit_cov = curve_fit(gauss_function, xdata = bins[0:300], ydata = values, p0 = p0, maxfev=5000)
-fitted_gauss = gauss_function(bins[0:300],*fit_gauss)
-
-print(fit_gauss)
-print(" sigma = " + str(fit_gauss[0])+ '+/-' +str (np.sqrt(fit_cov[0,0])))
-print(" mean = " + str(fit_gauss[1])+ '+/-' +str (np.sqrt(fit_cov[1,1])))
-print(" normalisation = " + str(fit_gauss[2])+ '+/-' +str (np.sqrt(fit_cov[2,2])))
-
-
-
-plt.plot(bins[0:300], fitted_gauss)
-plt.show()
-
-zoom = []
-for p in range(len(pix)):
-    if pix[p] >=4000: #and pix[p] <= 4000:
-        zoom.append(pix[p])
-
-plt.hist(zoom, bins = 100)
+#setting a cutting edge for the pixels
+for i in range(4611):
+    for j in range(2570): # For loop through whole image
+        impix = image[i][j] # Assign variable to pixel
+        if impix <= 3500: # Exclude values below chosen background
+            mask[i][j] = 0 # Change 1 to 0
 #%%
-mask_ones = np.ones((4611,2570))
-for i in range (len(image)):
-    for j in range (len(image[0])):
-        if image[i,j] <= 3455:
-            mask_ones[i,j] = 0
+pot_sources = [] # potential sources
+coor = []
+x_axis = []
+y_axis = []
+for i in range(4611):
+    for j in range(2570):
+        x = j
+        y = i
+        if mask[i][j] == 1:
+            x_axis.append(x)
+            y_axis.append(y)
+            coor.append([y,x])
+            pot_sources.append(image[i][j])
+            maximum = max(pot_sources)
+#print(maximum)   #check if the first 3 loops work       
+            for num in len(pot_sources):
+                if pot_sources[num] == maximum:
+                    x_centre = x_axis[num] - 1
+                    y_centre = y_axis[num] - 1
